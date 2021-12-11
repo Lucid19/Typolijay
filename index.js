9bedfc25dcfc9a95a44974b912cd91c5e2fb0c0d
Lucid19 <daumantasdauksas9@gmail.com>
2021-12-11 15:30:44 +0000

    heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// Package
 const Discord = require("discord.js")
 const config = require("./config.json")
 
+// Client
 const intents = new Discord.Intents(32767)
 const client = new Discord.Client({intents})
 
