27dd340574f9ae73dbd7b7d526eda358f7675e7d
Lucid19 <daumantasdauksas9@gmail.com>
2021-12-28 19:28:47 +0000

    fhsdfdh

--- commands/user.js
+++ commands/user.js
@@ -16,8 +16,8 @@ module.exports = {
             .setTitle(`${memberMen.username}'s profile`)
             .setColor("RANDOM")
             .setThumbnail(interaction.user.displayAvatarURL({dynamic: true}))
-            .addFields({name: "Joined", value: new Date(memberMen.joinedTimestamp).toLocaleDateString(), inline: true},
-                       {name: "Created", value: new Date(member.createdTimestamp).toLocaleDateString(), inline: true},
+            .addFields({name: "Joined", value: new Date(member.joinedTimestamp).toLocaleDateString(), inline: true},
+                       {name: "Created", value: new Date(memberMen.createdTimestamp).toLocaleDateString(), inline: true},
                        {name: "ID", value: String(memberMen.id)})
 
         interaction.reply({embeds: [userEmbed]})
