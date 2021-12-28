c870b8b530dda1b61b8e4ea3900ff329f0d575b1
Lucid19 <daumantasdauksas9@gmail.com>
2021-12-28 18:34:53 +0000

    asd

--- commands/user.js
+++ commands/user.js
@@ -6,13 +6,16 @@ module.exports = {
         .setName("user")
         .setDescription("Get your Stats and Information"),
     async execute(interaction){
+        user = interaction.user
+        member = interaction.guild.members.cache.get(user.id)
+
         const userEmbed = new MessageEmbed()
             .setTitle(`${interaction.user.username}'s profile`)
             .setColor("RANDOM")
             .setThumbnail(interaction.user.displayAvatarURL({dynamic: true}))
-            .addFields({name: "Joined", value: interaction.guild.member.joinedAt},
-                       {name: "Created", value: interaction.user.createdAt, inline: true},
-                       {name: "ID", value: interaction.user.id})
+            .addFields({name: "Joined", value: member.joinedAt},
+                       {name: "Created", value: user.createdAt, inline: true},
+                       {name: "ID", value: user.id})
 
         interaction.reply({embeds: [userEmbed]})
     }
