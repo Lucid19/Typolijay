8ac739735f7c11ab3fb6d3584dd981f942035a47
Lucid19 <daumantasdauksas9@gmail.com>
2022-01-01 21:16:09 +0000

    bruh

--- commands/user.js
+++ commands/user.js
@@ -50,7 +50,7 @@ module.exports = {
         collector.on('end', (ButtonInteraction) => {
             ButtonInteraction.first().deferUpdate()
             const id = ButtonInteraction.first().customId
-            if(id === "status") message[1].edit({embeds: [statEmbed]})
+            if(id === "status") console.log(message[0])
             else if(id === "back")return
         })
     }
