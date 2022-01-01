0e7b2cdb4b6cb76e85973eaa042366f4323dcb26
Lucid19 <daumantasdauksas9@gmail.com>
2022-01-01 17:36:39 +0000

    sgd

--- commands/user.js
+++ commands/user.js
@@ -38,7 +38,7 @@ module.exports = {
             return interaction.reply({content: "you are not the author", ephemeral: true})
         }
 
-        const collector = interaction.channel.createMessageComponentCollector({ filter })
+        const collector = interaction.channel.createMessageComponentCollector({ filter, max: 2 })
 
         collector.on('end', (ButtonInteraction) => {
             console.log(ButtonInteraction.first().customId)
