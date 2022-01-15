71f683b3ff1632b5e522586f87c52f22ce058b26
Lucid19 <daumantasdauksas9@gmail.com>
2022-01-15 21:22:19 +0000

    faf

--- commands/user.js
+++ commands/user.js
@@ -36,7 +36,7 @@ module.exports = {
             if(err) throw err
             console.log(member.id)
             for(let i=0; i < result.length; i++){
-                console.log(result[i].user_id)
+                console.log(result[i])
                 if(result[i].user_id === member.id) return setResult(result[i], results)
             }
         })
