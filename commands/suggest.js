51a980a43538a387e8c1efa9e8815cac25f264d3
Lucid19 <daumantasdauksas9@gmail.com>
2022-02-02 18:57:39 +0000

    poop

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,6 +1,6 @@
 // discord.js
 const{ SlashCommandBuilder} = require("@discordjs/builders")
-const { MessageEmbed, MessageActionRow, MessageSelectMenu, MessageButton } = require("discord.js")
+const { MessageEmbed, MessageActionRow, MessageSelectMenu, MessageButton, Message } = require("discord.js")
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -15,29 +15,33 @@ module.exports = {
             .setColor("RANDOM")
             .addFields({name: "Instructions", value: "Hi! Please look at the selection menus below  the embed and specify your suggestion, once you have specified all the fields, you'll be able to submit it!"})
 
-        const select = 
+        const select = new MessageActionRow()
+            .addOptions(
                 new MessageSelectMenu()
                     .setCustomId("type")
                     .setPlaceholder("Type")
                     .addOptions([
                         {label: "Suggestion", description: "What can we improve?", value: "suggestion"},
-                        {label: "Suggest topic", description: "would you like to suggest a topical question for future prompts?", value: "question"}])
+                        {label: "Suggest topic", description: "would you like to suggest a topical question for future prompts?", value: "question"}]))
         
-        const question = 
+        const question = new MessageActionRow()
+            .addOptions(
                 new MessageSelectMenu()
                     .setCustomId("category-suggestion")
                     .setPlaceholder("Category")
                     .addOptions([
                         {label: "Server", description: "What about the server?", value: "server-suggestion"},
                         {label: "Channels", description: "What about the channels?", value: "channels-suggestion"},
-                        {label: "Events", description: "regarding certain events to be held or discussed", value: "events-suggestion"}])
+                        {label: "Events", description: "regarding certain events to be held or discussed", value: "events-suggestion"}]))
 
-        const category = new MessageSelectMenu()
+        const category = new MessageActionRow()
+            .addOptions(
+                new MessageSelectMenu()
                     .setCustomId("category-topic")
                     .setPlaceholder("Category")
                     .addOptions([
                         { label: "Debate", description: "Suggest a topic", value: "debate-topic"},
-                        { label: "Server", description: "Any inquiries?", value: "server-inquiry"}])
+                        { label: "Server", description: "Any inquiries?", value: "server-inquiry"}]))
         
         const buttonRow = new MessageActionRow()
             .addComponents(
