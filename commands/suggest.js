1733b230264b3ca227236e58d4dd1d8a62923985
Lucid19 <daumantasdauksas9@gmail.com>
2022-02-02 18:48:54 +0000

    poop

--- commands/suggest.js
+++ commands/suggest.js
@@ -15,22 +15,15 @@ module.exports = {
             .setColor("RANDOM")
             .addFields({name: "Instructions", value: "Hi! Please look at the selection menus below  the embed and specify your suggestion, once you have specified all the fields, you'll be able to submit it!"})
 
-        const selectRow = new MessageActionRow()
-            .addComponents(
+        const select = 
                 new MessageSelectMenu()
                     .setCustomId("type")
                     .setPlaceholder("Type")
                     .addOptions([
                         {label: "Suggestion", description: "What can we improve?", value: "suggestion"},
-                        {label: "Suggest topic", description: "would you like to suggest a topical question for future prompts?", value: "question"}]),
-
-                new MessageSelectMenu()
-                    .setCustomId("category-topic")
-                    .setPlaceholder("Category")
-                    .addOptions([
-                        { label: "Debate", description: "Suggest a topic", value: "debate-topic"},
-                        { label: "Server", description: "Any inquiries?", value: "server-inquiry"}]),
-                
+                        {label: "Suggest topic", description: "would you like to suggest a topical question for future prompts?", value: "question"}])
+        
+        const question = 
                 new MessageSelectMenu()
                     .setCustomId("category-suggestion")
                     .setPlaceholder("Category")
@@ -38,22 +31,28 @@ module.exports = {
                         {label: "Server", description: "What about the server?", value: "server-suggestion"},
                         {label: "Channels", description: "What about the channels?", value: "channels-suggestion"},
                         {label: "Events", description: "regarding certain events to be held or discussed", value: "events-suggestion"}])
-        )
-        
-            const buttonRow = new MessageActionRow()
-                .addComponents(
-                    new MessageButton()
-                        .setLabel("Send")
-                        .setCustomId("sent")
-                        .setStyle("SECONDARY")
-                        .setDisabled(true),
 
-                    new MessageButton()
-                        .setLabel("X")
-                        .setCustomId("exit")
-                        .setStyle("DANGER")
+        const category = new MessageSelectMenu()
+                    .setCustomId("category-topic")
+                    .setPlaceholder("Category")
+                    .addOptions([
+                        { label: "Debate", description: "Suggest a topic", value: "debate-topic"},
+                        { label: "Server", description: "Any inquiries?", value: "server-inquiry"}]),
+        
+        const buttonRow = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setLabel("Send")
+                    .setCustomId("sent")
+                    .setStyle("SECONDARY")
+                    .setDisabled(true),
+
+                new MessageButton()
+                    .setLabel("X")
+                    .setCustomId("exit")
+                    .setStyle("DANGER")
         )
         
-        const message = interaction.reply({embeds: [suggestionEmbed], components: [selectRow[0]], fetchReply: true})
+        const message = interaction.reply({embeds: [suggestionEmbed], components: [select, buttonRow], fetchReply: true})
     }
 }
