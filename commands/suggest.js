9b958df13b736b22aacb9fb9b248812ea593bd94
Lucid19 <daumantasdauksas9@gmail.com>
2022-02-12 19:52:40 +0000

    woop

--- commands/suggest.js
+++ commands/suggest.js
@@ -20,14 +20,13 @@ module.exports = {
             .setColor("RANDOM")
             .addFields({name: "Instructions", value: "Hi! Please look at the selection menus below  the embed and specify your suggestion, once you have specified all the fields, you'll be able to submit it!"})
 
-        const selectMenus = new MessageActionRow()
-            .addComponents(
+        const selectMenus =
                 new MessageSelectMenu()
                     .setCustomId("type")
                     .setPlaceholder("Type")
                     .addOptions([
                         {label: "Suggestion", description: "What can we improve?", value: "suggestion"},
-                        {label: "Suggest topic", description: "would you like to suggest a topical question for future prompts?", value: "question"}]))
+                        {label: "Suggest topic", description: "would you like to suggest a topical question for future prompts?", value: "question"}])
         
         const suggestion = new MessageActionRow()
             .addComponents(
@@ -39,14 +38,13 @@ module.exports = {
                         {label: "Bot", description: "What about the bot?", value: "bot-suggestion"},
                         {label: "Events", description: "regarding certain events to be held or discussed", value: "events-suggestion"}]))
 
-        const question = new MessageActionRow()
-            .addComponents(
+        const question = 
                 new MessageSelectMenu()
                     .setCustomId("category-topic")
                     .setPlaceholder("Category")
                     .addOptions([
                         { label: "Debate", description: "Suggest a topic", value: "debate-topic"},
-                        { label: "Server", description: "Any inquiries?", value: "server-inquiry"}]))
+                        { label: "Server", description: "Any inquiries?", value: "server-inquiry"}])
         
         const buttonRow = new MessageActionRow()
             .addComponents(
