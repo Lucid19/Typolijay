ff18521b0eabd70f21d879308bbcdc83c0f45a73
Lucid19 <86158960+Lucid19@users.noreply.github.com>
2022-02-14 17:42:55 +0000

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -87,6 +87,7 @@ module.exports = {
 
             if(interaction.isSelectMenu()){
                 const value = interaction.values[0]
+                console.log(value)
 
                 if(id === "type" && value === "suggestion"){
                     buttonRow.components[0].setDisabled(true)
