77ff40450868218991533d22f871c1f10a6823dc
Lucid19 <86158960+Lucid19@users.noreply.github.com>
2022-07-21 19:34:09 +0100

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -15,77 +15,7 @@ module.exports = {
         const member = interaction.guild.members.cache.get(user.id)
         const tables = ["general", "debate", "motivational", "meme"]
 
-        const con = mysql.createConnection({
-            host: config.host,
-            user: config.user,
-            password: config.password,
-            database: config.database
-        })
-
-        const results = []
-
-        function setResult(level, messages){
-            results.push([level, messages])
-        }
-
-        function disable(){
-            row.components[0].setDisabled(true)
-            row.components[1].setDisabled(true)
-            row.components[2].setDisabled(true)
-            row.components[3].setDisabled(true)
-            message.edit({components: [row]})
-        }
-
-        function getResult(tableName) {
-            return new Promise((resolve, reject) => {
-                con.query(`SELECT * FROM ${tableName}`, (err, results) => {
-                    if(err) return reject(err)
-                    for(let i = 0; i < results.length; i++){
-                        if(results[i].user_id === member.id) return resolve(results[i])
-                    }
-                    return resolve(`User not found in ${tableName}`)
-                })
-            })
-        }
-
-        for(const table of tables){
-            try {
-                const result = await getResult(table)
-                if(result === "User not found"){
-                    console.log(`${result} in ${table}`)
-                }
-                else{
-                    setResult(result.level, result.messages)
-                }
-            } catch(err){
-                console.log(`Couldnt retreive data from ${table}: ${err}`)
-            }
-        }
-
-        function bar(level, messages){
-            function prog(max){
-                let bar = ":"
-                for(let i = 0; i < 10; i++){
-                    if(messages > max/10){
-                        bar = bar.concat(":blue_square:")
-                        messages -= max/10
-                    }
-                    else{
-                        bar = bar.concat(":black_large_square:")
-                    }
-                    
-                }
-                return bar
-            }
-
-            if(level === 0) return prog(500)
-            else if(level === 1) return prog(1000)
-            else if(level === 2) return prog(1500)
-            else if(level === 3) return prog(2000)
-            else if(level === 4) return prog(2500)
-            else if(level === 5) return "MAX"
-        }
-
+        
         // initial interface
         const userEmbed = new MessageEmbed()
             .setTitle(`${user.username}'s profile`)
@@ -105,10 +35,10 @@ module.exports = {
         const levelEmbed = new MessageEmbed()
             .setTitle(`${user.username}'s progress`)
             .setColor("RANDOM")
-            .addFields({name: "general", value: `lvl: ${results[0][0]} ${bar(results[0][0], results[0][1])} ${results[0][1]}`},
-                       {name: "debate", value: `lvl: ${results[1][0]} ${bar(results[1][0], results[1][1])} ${results[1][1]}`},
-                       {name: "motivational", value: `lvl: ${results[2][0]} ${bar(results[2][0], results[2][1])} ${results[2][1]}`},
-                       {name: "meme", value: `lvl: ${results[3][0]} ${bar(results[3][0], results[3][1])} ${results[3][1]}`})
+            .addFields({name: "general", value: "lvl: 3 :blue_square: :blue_square: :blue_square: :black_large_square: :black_large_square: 4"},
+                       {name: "debate", value: "lvl: 2 :blue_square: :blue_square: :blue_square: :black_large_square: :black_large_square: 3"},
+                       {name: "motivational", "lvl: 0 :blue_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: 1"},
+                       {name: "meme", value: "lvl: 1 :blue_square: :blue_square: :black_large_square: :black_large_square: :black_large_square: 2*")
 
         // buttons displayed
         const row = new MessageActionRow()
